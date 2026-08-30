2c0395ec171c73dac5ee4ef50b28ccc40f7b6177
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-08-29 22:21:56 -0300

    chore: adicionar estrutura inicial do backend

---
+++ backend/src/test/java/com/sonar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

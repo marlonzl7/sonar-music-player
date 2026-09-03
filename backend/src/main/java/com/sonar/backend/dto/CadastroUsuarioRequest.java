1c7a77b27d4f92fc98341cf82be4dadaa40dc2c6
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-09-03 00:08:10 -0300

    chore: remover DTOs antigos CadastroUsuario não removidos no rename

--- backend/src/main/java/com/sonar/backend/dto/CadastroUsuarioRequest.java
+++
@@ -1,6 +0,0 @@
-package com.sonar.backend.dto;
-
-public record CadastroUsuarioRequest(
-        String nome,
-        String email
-) {}
--- backend/src/main/java/com/sonar/backend/dto/CadastroUsuarioResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sonar.backend.dto;
-
-public record CadastroUsuarioResponse(
-        Long idUsuario,
-        String nome,
-        String email
-) {}
